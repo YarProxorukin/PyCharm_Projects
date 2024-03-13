02e242362e656fc48c4af49aa8901e8a713d8ad4
YarProxorukin <proxorukin.yar@mail.ru>
2024-03-13 12:46:30 +0300

    Initial commit

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -6,8 +6,7 @@ matrix = [[i + j * 3 + 1 for i in range(3)] for j in range(3)]
 last_index = len(matrix) - 1
 num_columns = len(matrix[0])
 
-for i in range(num_columns):
-    matrix[last_index][i] = 0
+matrix[last_index] = [0 for _ in range(num_columns)]
 
 for row in matrix:
     print(row)
--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -2,11 +2,12 @@
 В матрице элементы столбца N (N задать с клавиатуры) увеличить в два раза.
 """
 matrix = [[i + j * 3 + 1 for i in range(3)] for j in range(3)]
+for row in matrix:
+    print(row)
 
 number_column = int(input("Введите номер столбца (от 0 до 2): "))
 
-for row in matrix:
-    row[number_column] *= 2
+matrix = [[row[i] * 2 if i == number_column else row[i] for i in range(len(row))] for row in matrix]
 
 for row in matrix:
     print(row)
