3a45f61c398257e6b5b82603a74689ecc8faf73b
YarProxorukin <proxorukin.yar@mail.ru>
2024-03-13 12:55:12 +0300

    Initial commit

--- PZ_13/PZ_13_1.py
+++ PZ_13/PZ_13_1.py
@@ -2,6 +2,8 @@
 В матрице элементы последней строки заменить на 0.
 """
 matrix = [[i + j * 3 + 1 for i in range(3)] for j in range(3)]
+for row in matrix:
+    print(row)
 
 last_index = len(matrix) - 1
 num_columns = len(matrix[0])
